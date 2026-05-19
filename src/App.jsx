b5165428a33ef997136ca28e6c792af9ccc80ed5
Darael Eri Gaddiel <daraeleri@gmail.com>
2026-05-19 18:09:46 +0100

    feat: add React and Vite setup with Tailwind CSS integration
    - Added React and Vite configuration files.
    - Created main entry point for React application.
    - Included Tailwind CSS in the project.
    - Added SVG assets for React and Vite logos.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-2xl font-bold text-red-500'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
